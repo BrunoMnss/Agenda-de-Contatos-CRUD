05dc52c2cd5a5c3a703819d6d7f79e848b78210a
Bruno Moreira <68245780+BrunoMnss@users.noreply.github.com>
2023-06-03 11:27:44 -0300

    Update App.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-cinza shadow-sm">
             <div class="container">
-                <a class="navbar-brand text-light" href="{{ route('home') }}"> 
+                <a class="navbar-brand text-light" href="{{ route('home') }}">
                     Agenda de contatos
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
